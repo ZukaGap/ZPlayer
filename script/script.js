80f19bcf547bc4a5c378e27639252d422134bcf6
zuka <zurabigaprindashvili@gmail.com>
2019-09-22 18:29:31 +0400

    added time, current time

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 var video = document.querySelector(".video");
 var juice = document.querySelector(".orange-juice");
 var btn = document.getElementById("play-pause");
+currentTime = document.getElementById("current-time");
+fullTime = document.getElementById("full-time");
 
 function togglePlayPause()
 {
@@ -23,4 +25,12 @@ btn.onclick = function() {
 video.addEventListener("timeupdate", function(){
     var juicePos=video.currentTime / video.duration;
     juice.style.width = juicePos * 100 + "%";
+    //*** mimdinare dro ***//
+    liveTime = Math.floor(video.currentTime);
+    liveTime = parseInt(liveTime / 60 , 10) + ":" + liveTime % 60;
+    currentTime.innerHTML = liveTime + " /";
+    //*** mtliani dro ****//
+    fltm = Math.floor(video.duration);
+    fltm = parseInt(fltm / 60 , 10) + ":" + fltm % 60;
+    fullTime.innerHTML = fltm;
 })
